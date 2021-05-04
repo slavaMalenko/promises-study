0b9ca1f249300a04f7572abe81e316b149778400
slavaMalenko <denouement20@yandex.ru>
2021-05-04 23:12:04 +0300

    promises with no input from outside

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-const family = [
-    { member: 'mother', id: 1, coffee: 'Latte' },
-    { member: 'father', id: 2, coffee: 'Espresso' },
-    { member: 'son', id: 3, coffee: 'Cortado' }
-]
+console.log('Request data...')
 
-const getCoffee = (member) => {
-    const coffeePromise = fetch('https://api.sampleapis.com/coffee/hot');
-    return coffeePromise
-        .then(data => data.json())
-        .then(list => {
-            const coffee = list.find(res => res.title === member.coffee);
-            return {
-                ...member, coffee
-            }
-        })
-}
+const a = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Preparing data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000)
+})
 
-const getFamilyMember = (id) => {
-    return new Promise((resolve, reject) => {
+a.then(data => {
+    return b = new Promise((resolve, reject) => {
         setTimeout(() => {
-            const member = family.find(res => res.id === id);
-            if (member) {
-                resolve(member);
-            } else {
-                reject(Error('Член семьи не найден'))
-            }
-        }, 1500)
+            data.modified = true
+            resolve(data)
+        }, 2000)
     })
-}
-
-getFamilyMember(4)
-    .then(data => getCoffee(data))
-    .then(newMember => console.log(newMember))
-    .catch(err => console.log(err))
+})
+    .catch(err => console.log('Error: ', err))
+    .then(clientData => {
+        clientData.fromPromise = true
+        return clientData
+    })
+    .then(data => {
+        console.log('Modified', data)
+    })
+    .finally(() => console.log('Finally!'))
